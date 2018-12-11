89e323ea5eabfb9bb7d9541fd5b06a8168a5fdd2
Alex Alvarez Gárciga <aagarciga@gmail.com>
2018-12-11 15:11:47 -0300

    Added Application Role Added Application DBContext Added EntityFrameworkCore dependencies Added PostgreSQL dependencies

---
+++ Beattle.Persistence.PostgreSQL/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Beattle.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beattle.Persistence.PostgreSQL
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+    }
+}
