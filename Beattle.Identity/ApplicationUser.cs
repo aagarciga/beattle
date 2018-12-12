247d632eeac85c9d79d98c0e700f215056b6983d
Alex Alvarez Gárciga <aagarciga@gmail.com>
2018-12-12 11:47:10 -0300

    Added Roles and Claims navegation properties to ApplicationUser

--- Beattle.Identity/ApplicationUser.cs
+++ Beattle.Identity/ApplicationUser.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
 
 namespace Beattle.Identity
 {
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Navigation property for the roles user belongs to
+        /// </summary>
+        public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
+
+        /// <summary>
+        /// Navigation property for the claims this user possesses
+        /// </summary>
+        public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
     }
 }
