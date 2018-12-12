7b378122a4c539f1f3784f8a3860314f0093a5d4
Alex Alvarez Gárciga <aagarciga@gmail.com>
2018-12-12 14:34:12 -0300

    Added ApplicationClaimType for custom application security related claims

---
+++ Beattle.Identity/ApplicationClaimType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beattle.Identity
+{
+    public static class ApplicationClaimType
+    {
+        /// <summary>
+        /// A claim that specifies the permission of an entity
+        /// </summary>
+        public const string Permission = "permission";
+
+        /// <summary>
+        /// A claim that specifies the settings of an entity
+        /// </summary>
+        public const string Setings = "settings";
+    }
+}
