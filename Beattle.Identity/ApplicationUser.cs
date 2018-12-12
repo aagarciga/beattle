69d84df642c4810948eb8d92cb16c6dffb7d32a5
Alex Alvarez Gárciga <aagarciga@gmail.com>
2018-12-12 12:09:46 -0300

    Added Constructor to Application user in order to initialize the Roles and Claims collections using HashSets

--- Beattle.Identity/ApplicationUser.cs
+++ Beattle.Identity/ApplicationUser.cs
@@ -7,6 +7,18 @@ namespace Beattle.Identity
     public class ApplicationUser : IdentityUser
     {
         /// <summary>
+        /// Initializes a new instance of <see cref="ApplicationUser"/>
+        /// </summary>
+        /// <remarks>
+        /// The Id property is initialized to from a new GUID string value
+        /// </remarks>
+        public ApplicationUser()
+        {
+            Roles = new HashSet<IdentityUserRole<string>>();
+            Claims = new HashSet<IdentityUserClaim<string>>();
+        }
+
+        /// <summary>
         /// Navigation property for the roles user belongs to
         /// </summary>
         public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
