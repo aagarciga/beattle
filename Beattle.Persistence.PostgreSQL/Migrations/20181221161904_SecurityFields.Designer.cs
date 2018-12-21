72e8d0d4da3ef27927956a18a9a5dc3790f95bd2
Alex Alvarez Gárciga <aagarciga@gmail.com>
2018-12-21 13:20:51 -0300

    Migration for New Identity Fields

--- Beattle.Persistence.PostgreSQL/Migrations/20181221161904_SecurityFields.Designer.cs
+++ Beattle.Persistence.PostgreSQL/Migrations/20181221161904_SecurityFields.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Beattle.Persistence.PostgreSQL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Beattle.Persistence.PostgreSQL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181221161904_SecurityFields")]
+    partial class SecurityFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Beattle.Persistence.PostgreSQL/Migrations/20181221161904_SecurityFields.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Beattle.Persistence.PostgreSQL.Migrations
+{
+    public partial class SecurityFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Settings",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Role",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Settings",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Role");
+        }
+    }
+}
